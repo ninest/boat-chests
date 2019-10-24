caeb8d2dcc49e1e7ab77b53511c647c6bcf561ea
themindstorm <parth.kabra@gmail.com>
2019-10-24 13:15:50 +0800

    1 commit

--- data/tm-boat-chests/functions/commands.py
+++ data/tm-boat-chests/functions/commands.py
@@ -14,5 +14,5 @@ boat_types = ["oak", "spruce", "birch", "jungle", "acacia", "dark_oak"]
 for type in boat_types:
   print('execute as @e[type=item,nbt={Item:{id:"minecraft:chest_minecart"}}] at @s if entity @e[type=minecraft:boat,nbt={Type:"' + type + '"},sort=nearest,distance=..1,tag=!chestboat_' + type + '] run tag @s add chestboat_item_' + type)
   print(f'execute at @e[tag=chestboat_item_{type}] run kill @e[type=minecraft:boat,sort=nearest,distance=..1,tag=!chestboat]')
-  print('execute at @e[tag=chestboat_item_' + type + '] run summon minecraft:boat ~ ~1 ~ {Type:"' + type + '",Tags:["chestboat_' + '", "chestboat"],Passengers:[{id:"minecraft:chest_minecart",Tags:["chest"]}]}')
+  print('execute at @e[tag=chestboat_item_' + type + '] run summon minecraft:boat ~ ~1 ~ {Type:"' + type + '",Tags:["chestboat_' + type + '", "chestboat"],Passengers:[{id:"minecraft:chest_minecart",Tags:["chest"]}]}')
   print(f'kill @e[tag=chestboat_item_{type}]')
